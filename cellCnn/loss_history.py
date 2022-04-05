30f070335a9663a14f6fde650124e4b76d6b04a7
irrelifant <eliasschreiner@gmx.de>
2022-04-05 09:44:20 +0200

    bugfix: loss_history for STL

--- cellCnn/loss_history.py
+++ cellCnn/loss_history.py
@@ -2,6 +2,7 @@ import os
 import keras.callbacks
 from matplotlib import pyplot as plt
 
+#todo extend this further
 class LossHistory(keras.callbacks.Callback):
     def __init__(self, outdir='/default_outdir', irun=0):
         self.irun = irun
@@ -13,19 +14,21 @@ class LossHistory(keras.callbacks.Callback):
         self.losses = []
 
     def on_epoch_end(self, epoch, logs=None):
-        sigmas = [s.numpy() for s in self.model.layers[-1].get_sigmas()]
-        self.sigmas.append(sigmas)
+        if hasattr(self.model.layers[-1], 'get_sigmas'):
+            sigmas = [s.numpy() for s in self.model.layers[-1].get_sigmas()]
+            self.sigmas.append(sigmas)
         self.losses.append(logs)
 
     def on_train_end(self, logs=None):
         plt.clf()  # clear
 
-        for i, _ in enumerate(self.sigmas[0]):
-            sigmas = [s[i] for s in self.sigmas]
-            plt.plot(range(len(sigmas)), sigmas,
-                     label=f'sigma {i}')
-        plt.title(f'Sigmas along epochs')
-        plt.xlabel(f'Epochs')
-        plt.ylabel('Sigma')
-        plt.legend()
-        plt.savefig(f'{self.plotdir}/sigma_plot_{self.irun}.png')
+        if len(self.sigmas) > 0:
+            for i, _ in enumerate(self.sigmas[0]):
+                sigmas = [s[i] for s in self.sigmas]
+                plt.plot(range(len(sigmas)), sigmas,
+                         label=f'sigma {i}')
+            plt.title(f'Sigmas along epochs')
+            plt.xlabel(f'Epochs')
+            plt.ylabel('Sigma')
+            plt.legend()
+            plt.savefig(f'{self.plotdir}/sigma_plot_{self.irun}.png')
