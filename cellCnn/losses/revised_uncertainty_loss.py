2f8d2848ee0a5c4051dc1ca014c43f862dd03213
irrelifant <eliasschreiner@gmx.de>
2022-08-29 23:08:46 +0200

    loss layers cleanup

--- cellCnn/losses/revised_uncertainty_loss.py
+++ cellCnn/losses/revised_uncertainty_loss.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-
 from keras import backend as K
 from keras.layers import Layer
 
@@ -9,48 +8,41 @@ Author: Elias Schreiner
 
 This is a custom layer that should implement the Revised Uncertainty Loss (https://github.com/Mikoto10032/AutomaticWeightedLoss) 
 which is an extension to the uncertainty weighting by Kendall (https://github.com/ranandalon/mtl)
-
-Open Points:
-- check if metric added works as expected
-- check if we can mute the losses from the 2 input dense layers (as this layer should cover those 2)
-    - I assume that this needs further deepdive
 """
 
 class RevisedUncertaintyLossV2(Layer):
     def __init__(self, loss_list, **kwargs):
         self.loss_list = loss_list
-        self.sigmas = []
+        self.sigmas_sq = []
         super(RevisedUncertaintyLossV2, self).__init__(**kwargs)
 
     def build(self, input_shape=None):
-        self.sigmas = []
-        sigma_init = tf.random_uniform_initializer(minval=0.2, maxval=1.)
-        const_init = tf.keras.initializers.Constant(3.)
+        self.sigmas_sq = []
+        const_init = tf.keras.initializers.Constant(1 / len(self.loss_list))
         for i in range(len(self.loss_list)):
-            self.sigmas += [self.add_weight(name=f'sigma_{i}', shape=(1,),
-                                           initializer=const_init,
-                                           trainable=True)]
+            self.sigmas_sq += [self.add_weight(name=f'sigma_{i}', shape=(1,),
+                                               initializer=const_init,
+                                               trainable=True)]
         super(RevisedUncertaintyLossV2, self).build(input_shape)
 
     def get_sigmas(self):
-        return self.sigmas
+        return self.sigmas_sq
 
     def get_mtl_loss(self, ys_true, ys_pred):
-        print('in mtl: split inputs')
-        assert len(ys_true) == len(self.sigmas)
-        assert len(ys_pred) == len(self.sigmas)
+        assert len(ys_true) == len(self.sigmas_sq)
+        assert len(ys_pred) == len(self.sigmas_sq)
         loss = 0.
-        for y_true, y_pred, sigma, task_loss in zip(ys_true, ys_pred, self.sigmas, self.loss_list):
-            sigma_sq = tf.pow(sigma[0], 2)
+        for y_true, y_pred, sigma_sq, task_loss in zip(ys_true, ys_pred, self.sigmas_sq, self.loss_list):
             factor = tf.math.divide_no_nan(1.0, tf.multiply(2.0, sigma_sq))
             listed_loss = task_loss[0](y_true, y_pred)
-            loss += tf.add(factor * listed_loss, tf.math.log(1 + sigma_sq))
+            tf.print(sigma_sq, listed_loss, factor, tf.math.log(1 + sigma_sq), sep='_;_')
+            loss += K.sum(factor * listed_loss + tf.math.log(1 + sigma_sq))
         return K.mean(loss)
 
     def get_config(self):
         config = super().get_config()
         # https://github.com/tensorflow/tensorflow/issues/28799 describes that you can only serialize numpys when saving the model (later we will save...)
-        sigmas_to_save = [s.numpy() for s in self.sigmas]
+        sigmas_to_save = [s.numpy() for s in self.sigmas_sq]
         config.update({
             "sigmas": sigmas_to_save,
             "loss_list": self.loss_list
@@ -58,10 +50,10 @@ class RevisedUncertaintyLossV2(Layer):
         return config
 
     def call(self, inputs, *args, **kwargs):
-        print('CALLING...')
-        ys_true = inputs[:len(self.sigmas)]
-        ys_pred = inputs[len(self.sigmas):]
+        ys_true = inputs[:len(self.sigmas_sq)]
+        ys_pred = inputs[len(self.sigmas_sq):]
         loss = self.get_mtl_loss(ys_true, ys_pred)
         self.add_loss(loss, inputs=inputs)
         self.add_metric(loss, name='revised_uncertainty_loss')
+        #[print(sigma_sq) for sigma_sq in self.sigmas_sq]
         return ys_pred
--- cellCnn/losses/uncertainty_loss.py
+++ cellCnn/losses/uncertainty_loss.py
@@ -3,32 +3,39 @@
 ## refered from https://towardsdatascience.com/deep-multi-task-learning-3-lessons-learned-7d0193d71fd6
 ##
 from keras import backend as K
-from keras.initializers.initializers_v2 import Constant
 from keras.layers import Layer
 import tensorflow as tf
+
 
 # Custom loss layer
 class UncertaintyMultiLossLayer(Layer):
     def __init__(self, loss_list=None, nb_outputs=2, **kwargs):
         self.loss_list = loss_list
         self.nb_outputs = nb_outputs
+        self.log_vars = []  # container of log sigma squares
         super(UncertaintyMultiLossLayer, self).__init__(**kwargs)
 
     def build(self, input_shape=None):
-        self.log_vars = [] # container of log sigma squares
+        const_init = tf.keras.initializers.Constant(1 / self.nb_outputs)
+
         for i in range(self.nb_outputs):
             self.log_vars += [self.add_weight(name='log_var' + str(i), shape=(1,),
-                                              initializer=tf.keras.initializers.Constant(0.),
+                                              initializer=const_init,
                                               trainable=True)]
         super(UncertaintyMultiLossLayer, self).build(input_shape)
 
+    def get_sigmas(self):
+        return self.log_vars
+
     def multi_loss(self, ys_true, ys_pred):
         assert len(ys_true) == self.nb_outputs
+        assert len(ys_pred) == self.nb_outputs
         loss = 0
-        for y_true, y_pred, log_variance, task_loss in zip(ys_true, ys_pred, self.log_vars, self.loss_list):
-            precision = K.exp(-log_variance[0]) # resolved to the positive domain giving valid values for variance.
+        for y_true, y_pred, log_var, task_loss in zip(ys_true, ys_pred, self.log_vars, self.loss_list):
+            precision = K.exp(-log_var[0])  # resolved to the positive domain giving valid values for variance.
             listed_loss = task_loss[0](y_true, y_pred)
-            loss += tf.add(precision * listed_loss + log_variance[0], -1)
+            tf.print(precision, listed_loss, log_var[0], sep='_;_')
+            loss += K.sum(precision * listed_loss + log_var[0])
         return K.mean(loss)
 
     def get_config(self):
@@ -46,4 +53,5 @@ class UncertaintyMultiLossLayer(Layer):
         loss = self.multi_loss(ys_true, ys_pred)
         self.add_loss(loss, inputs=inputs)
         self.add_metric(loss, name='uncertainty_loss')
+        [print(log_var) for log_var in self.log_vars]
         return ys_pred
